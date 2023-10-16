640d3d5e8e6e4b2ad9f54044348a4ac98b33c41f
Hugo Porfirio <66637650+paezitow@users.noreply.github.com>
2023-10-16 12:13:10 -0300

    Update IUserRepository.java

--- src/main/java/br/com/hp/todolist/Repository/IUserRepository.java
+++ src/main/java/br/com/hp/todolist/Repository/IUserRepository.java
@@ -3,11 +3,13 @@ package br.com.hp.todolist.Repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.hp.todolist.Model.UserModel;
 
-
+@Repository
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
     UserModel findByUsername(String username);
 
 }
+
