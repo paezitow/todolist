9cbd1f03c5cd07d971ef1e9a1c73d5d1a524df6e
Hugo Porfirio <66637650+paezitow@users.noreply.github.com>
2023-10-16 12:12:54 -0300

    Update ITaskRepository.java

--- src/main/java/br/com/hp/todolist/Repository/ITaskRepository.java
+++ src/main/java/br/com/hp/todolist/Repository/ITaskRepository.java
@@ -3,12 +3,14 @@ package br.com.hp.todolist.Repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import br.com.hp.todolist.Model.TaskModel;
 
+@Repository
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
     
-    List<TaskModel> findByIdUser(UUID userId);
-    TaskModel findByIdanIdUser(UUID idTask, UUID userId);
+    List<TaskModel> findByIdUser(UUID idUser);
+    TaskModel findByIdAndIdUser(UUID id, UUID idUser);
 }
