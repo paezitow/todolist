276144cfa1c86c0e2049ca5e59c35bac2f97d3bc
Hugo Porfirio <66637650+paezitow@users.noreply.github.com>
2023-10-16 15:40:02 -0300

    Update TodolistApplication.java

--- src/main/java/br/com/hp/todolist/TodolistApplication.java
+++ src/main/java/br/com/hp/todolist/TodolistApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("br.com.hp.todolist.Filter")
+@ComponentScan("br.com.hp.todolist.Controller")
 @SpringBootApplication
 public class TodolistApplication {
 
